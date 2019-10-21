04430f59eefe13429e021d8197b2f6c0cdddda86
Alex Scarlett <alex.scarlett91@gmail.com>
2019-10-21 03:34:01 -0400

    Final working iteration that successfully creates a container on the local machine
    Rewrite 'run' function to be a fork and exec before the syscall clone flags. Add clone flag for PID along with UTS. Create 'child' function to use the separate file system and add 'child' function as part of the switch statement in 'main' function.

--- main.go
+++ main.go
@@ -9,40 +9,70 @@ import (
 	"syscall"
 )
 
+// docker run <container> <cmd> <args>
+// go run main.go run <cmd> <args>
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("what???")
 	}
 }
 
-// Sets up our command and then we are calling Run to actually run it
 func run() {
-	fmt.Printf("running %v\n", os.Args[2:])
+	/*
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+		Running "/proc/self/exe" is basically running a fork and exec
+
+		fork and exec --> 'fork' starts a new process which is a copy of the
+		one that calls it, while 'exec' replaces the current process image
+		with another (different) one
+
+	*/
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	/*
+
 		SysProcAttr holds optional, operating system-specific attributes.
 		Run passes it to os.StartProcess as the os.ProcAttr's Sys field.
 
 		SysProcAttr is a struct that has a field of 'Cloneflags' that is
 		of type 'uintptr' and it flags for clone calls (only on Linux).
 
-		We're only going to pass in one flag and it is 'syscall.CLONE_NEWUTS'.
-		This will clone a new UTS (Unix Time Sharing), which is the
-		namespace that specifically isolates the hostname.
+		What we are trying to clone here are different namespaces. The first
+		is UTS (UNIX Time Sharing) that deals with changing the hostname.
+
+		The second is PID (process ID) which provides processes with an independent
+		set of process IDs (PIDs) from other namespaces.
+
 	*/
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Use the separate root file system
+	must(syscall.Chroot("/home/rootfs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	must(cmd.Run())
 }
 
